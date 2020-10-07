9422939c07cf104f47218f75f3b8221c74c8cbc5
lalag <lalagvanan@gmail.com>
2020-10-07 14:17:22 +0300

    ValidateAndThrowCustom works correctly now

--- src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
+++ src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
@@ -9,23 +9,19 @@ namespace LT.DigitalOffice.Kernel.FluentValidationExtensions
     public static class ValidateAndThrowExtension
     {
         /// <summary>
-        /// Handle exceptions.
-        /// </summary>
-        /// <param name="validator"></param>
-        /// <param name="instance"></param>
-        /// <param name="ruleSet"></param>
-        /// <returns>Asynchronous operation for handling exceptions.</returns>
+		/// Performs validation and then throws an exception if validation fails.
+		/// </summary>
+		/// <param name="validator">The validator this method is extending.</param>
+		/// <param name="instance">The instance of the type we are validating.</param>
+		/// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
         [System.Obsolete]
-        public static void ValidateAndThrow<T>(this IValidator<T> validator, T instance, string ruleSet = null)
+        public static void ValidateAndThrowCustom<T>(this IValidator<T> validator, T instance, string ruleSet = null)
         {
             var result = validator.Validate(instance, ruleSet: ruleSet);
 
             if (!result.IsValid)
             {
-                var messages = result.Errors.Select(x => x.ErrorMessage);
-                string message = messages.Aggregate((x, y) => x + "\n" + y);
-
-                throw new ValidationException(message);
+                throw new ValidationException(result.Errors.ToString());
             }
         }
     }
