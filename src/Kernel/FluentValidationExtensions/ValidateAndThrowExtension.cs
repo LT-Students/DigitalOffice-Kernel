dd649895b2c1ac1c436919f97951c3c1c2b538e6
Lena <59228285+lalagvana@users.noreply.github.com>
2020-10-08 23:52:29 +0300

    ValidateAndThrow extension (#13)
    * Added ValidateAndThrow logic
    
    * ValidateAndThrowCustom works correctly now
    
    * Update Kernel.csproj
    
    * Update ValidateAndThrowExtension.cs
    
    * Upgraded using of Validate method
    
    * Update ValidateAndThrowExtension.cs
    
    * Update ValidateAndThrowExtension.cs

---
+++ src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System.Linq;
+
+namespace LT.DigitalOffice.Kernel.FluentValidationExtensions
+{
+    /// <summary>
+    /// Provides ValidateAndThrow method for extend FluentValidation package.
+    /// </summary>
+    public static class ValidateAndThrowExtension
+    {
+        /// <summary>
+		/// Performs validation and then throws an exception if validation fails.
+		/// </summary>
+		/// <param name="validator">The validator this method is extending.</param>
+		/// <param name="instance">The instance of the type we are validating.</param>
+		/// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
+        public static void ValidateAndThrowCustom<T>(this IValidator<T> validator, T instance, string ruleSet = null)
+        {
+            var result = validator.Validate(instance, options => options.IncludeRuleSets(ruleSet));
+
+            if (result != null && !result.IsValid)
+            {
+                throw new ValidationException(string.Join("\n", result.Errors));
+            }
+        }
+    }
+}
