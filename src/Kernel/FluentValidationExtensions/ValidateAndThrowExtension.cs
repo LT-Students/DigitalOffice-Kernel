6a8db626735e8baedab9cb3de38bba7a79116d8e
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-10-09 21:16:27 +0300

    Updated ValidateAndThrowCustom

--- src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
+++ src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
@@ -16,7 +16,7 @@ namespace LT.DigitalOffice.Kernel.FluentValidationExtensions
         /// <param name="ruleSets">Optional: rulesets when need to validate against.</param>
         public static void ValidateAndThrowCustom<T>(this IValidator<T> validator, T instance, params string[] ruleSets)
         {
-            var result = validator.Validate(instance, options => { if (ruleSets.Any()) { options.IncludeRuleSets(ruleSets); } });
+            var result = validator.Validate(instance, options => { if (ruleSets != null && ruleSets.Any()) { options.IncludeRuleSets(ruleSets); } });
 
             if (result != null && !result.IsValid)
             {
