a57b89f984c154fd47f190e4ca4fbea8f1ec04e4
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-10-09 12:51:27 +0300

    Removed ruleSet

--- src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
+++ src/Kernel/FluentValidationExtensions/ValidateAndThrowExtension.cs
@@ -13,10 +13,9 @@ namespace LT.DigitalOffice.Kernel.FluentValidationExtensions
 		/// </summary>
 		/// <param name="validator">The validator this method is extending.</param>
 		/// <param name="instance">The instance of the type we are validating.</param>
-		/// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
-        public static void ValidateAndThrowCustom<T>(this IValidator<T> validator, T instance, string ruleSet = null)
+        public static void ValidateAndThrowCustom<T>(this IValidator<T> validator, T instance)
         {
-            var result = validator.Validate(instance, options => options.IncludeRuleSets(ruleSet));
+            var result = validator.Validate(instance);
 
             if (result != null && !result.IsValid)
             {
