5e97764839d542ee500f81eeb8112382c199060e
Spartak Ryabtsev <spartak.ryabtsev@lanit-tercom.com>
2020-10-28 15:47:37 +0300

    Update comments

--- src/Kernel/DependencyInjection/ServiceCollectionExtension.cs
+++ src/Kernel/DependencyInjection/ServiceCollectionExtension.cs
@@ -7,16 +7,26 @@ using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Helper class for services extensions.
+    /// </summary>
     public static class ServiceCollectionExtension
     {
+        /// <summary>
+        /// Add <see cref="AccessValidator"/> and HttpContextAccessor.
+        /// </summary>
         public static IServiceCollection AddKernelExtensions(this IServiceCollection services)
         {
             services.AddHttpContextAccessor();
+
             services.AddTransient<IAccessValidator, AccessValidator>();
 
             return services;
         }
 
+        /// <summary>
+        /// Add requst clients used by <see cref="AccessValidator"/>.
+        /// </summary>
         public static IServiceCollectionBusConfigurator ConfigureKernelMassTransit(
             this IServiceCollectionBusConfigurator busConfigurator,
             RabbitMQOptions rabbitmqOptions)
