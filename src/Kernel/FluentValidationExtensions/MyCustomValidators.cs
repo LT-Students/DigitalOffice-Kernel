c9131ee213dbf1f47cc41c16a0978c8df48f5e89
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-12-04 18:04:40 +0300

    Add custom validation rule for path (#16)
    * Add custom validation rule for path
    
    * Update message

---
+++ src/Kernel/FluentValidationExtensions/MyCustomValidators.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LT.DigitalOffice.Kernel.FluentValidationExtensions
+{
+    /// <summary>
+    /// Custom validation rules.
+    /// </summary>
+    public static class MyCustomValidators
+    {
+        /// <summary>
+        /// Checks that an operation that is unique in a path has an allowed operation.
+        /// If there is no operation with this path, a <see cref="NullReferenceException"/> will be thrown.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity being validated. Usually JsonPatchDocument.</typeparam>
+        /// <typeparam name="TEntity">Operation.</typeparam>
+        /// <param name="ruleBuilder">Rule builder.</param>
+        /// <param name="path">Path to changeable value.</param>
+        /// <param name="allowedOps">Permitted operations.</param>
+        /// <returns>Another rule in the chain of rules.</returns>
+        public static IRuleBuilderOptions<T, IList<TEntity>> UniqueOperationWithAllowedOp<T, TEntity>(
+            this IRuleBuilder<T, IList<TEntity>> ruleBuilder,
+            string path,
+            params string[] allowedOps) where TEntity : Operation
+        {
+            return ruleBuilder.Must(x => allowedOps.Contains(x.FirstOrDefault(x => x.path == path).op))
+            .WithMessage($"Your operation with {path} not allowed. " +
+            $"Allowed ops: {new StringBuilder().AppendJoin(", ", allowedOps)}");
+        }
+    }
+}
