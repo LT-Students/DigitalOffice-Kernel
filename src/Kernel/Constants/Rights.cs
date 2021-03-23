b4907157cb03a35b9e8e97dcdc76467ceded136b
Mariya <mariyasin@list.ru>
2021-03-23 23:16:31 +0300

    added constants

--- src/Kernel/Constants/Rights.cs
+++ src/Kernel/Constants/Rights.cs
@@ -2,6 +2,9 @@
 {
     public static class Rights
     {
+        public const int AddEditRemoveUsers = 1;
+        public const int AddEditRemoveProjects = 2;
+        public const int AddEditRemoveEmailTemplates = 3;
         public const int AddEditRemoveDepartments = 4;
     }
 }
