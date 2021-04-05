83769cf254d32cfb9d051492048476802798254d
Spartak Ryabtsev <spartak.r@gmail.com>
2021-04-05 19:45:18 +0300

    add nuspec. add api info

---
+++ src/Kernel/ApiInformation/ApiInfoResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace LT.DigitalOffice.Kernel.ApiInformation
+{
+    public class ApiInfoResponse
+    {
+        public string Version { get; init; }
+        public string UpTime { get; init; }
+    }
+}
---
+++ src/Kernel/ApiInformation/BaseApiInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LT.DigitalOffice.Kernel.ApiInformation
+{
+    public abstract class BaseApiInfo
+    {
+        public abstract string Version { get; init; }
+        public abstract DateTime StartTime { get; init; }
+    }
+}
