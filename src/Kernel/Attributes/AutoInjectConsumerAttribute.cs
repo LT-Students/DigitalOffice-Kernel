aed0b78cf085ad87ac2d8f50fa3f0c9c7ebe1f28
KostinP <>
2021-04-09 02:20:33 +0300

    Add auto inject consumer extension

---
+++ src/Kernel/Attributes/AutoInjectConsumerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LT.DigitalOffice.Kernel.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AutoInjectConsumerAttribute : Attribute
+    {
+        public string EndpointPropertyName { get; init; }
+
+        public AutoInjectConsumerAttribute(
+            string endpointPropertyName)
+        {
+            EndpointPropertyName = endpointPropertyName;
+        }
+    }
+}
--- src/Kernel/Extensions/MassTransitExtensions.cs
+++ src/Kernel/Extensions/MassTransitExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using LT.DigitalOffice.Kernel.Attributes;
 using LT.DigitalOffice.Kernel.Configurations;
+using MassTransit;
 using MassTransit.ExtensionsDependencyInjectionIntegration;
+using MassTransit.RabbitMqTransport;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -77,6 +79,81 @@ namespace LT.DigitalOffice.Kernel.Extensions
 
                 logger?.LogTrace(
                     $"Found request type '{injectInterface.Name}'. Successfully injected with endpoint '{endpointUri}' and timeout '{attr.Timeout.Value}'.");
+            }
+
+            return busConfigurator;
+        }
+
+        public static IServiceCollectionBusConfigurator AddDoConsumers(
+            this IServiceCollectionBusConfigurator busConfigurator,
+            BaseRabbitMqConfig rabbitMqConfig,
+            IBusRegistrationContext context,
+            IRabbitMqBusFactoryConfigurator configurator,
+            ILogger logger = null)
+        {
+            if (busConfigurator == null)
+            {
+                return busConfigurator;
+            }
+
+            if (rabbitMqConfig == null)
+            {
+                throw new ArgumentNullException(nameof(rabbitMqConfig));
+            }
+
+            var asmPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+            var files = Directory.GetFiles(asmPath, "*DigitalOffice*.dll");
+
+            List<Assembly> assemblies = new();
+
+            foreach (string fileName in files)
+            {
+                assemblies.Add(Assembly.LoadFrom(fileName));
+            }
+
+            List<Type> injectConsumers = new();
+
+            foreach (Assembly assembly in assemblies)
+            {
+                injectConsumers.AddRange(assembly.ExportedTypes
+                    .Where(
+                        t =>
+                            t.IsClass
+                            && t.IsPublic
+                            && t.GetCustomAttribute(typeof(AutoInjectConsumerAttribute)) != null)
+                    .ToList());
+            }
+
+            foreach (Type injectConsumer in injectConsumers)
+            {
+                var attr = injectConsumer.GetCustomAttribute<AutoInjectConsumerAttribute>();
+
+                PropertyInfo property = rabbitMqConfig
+                    .GetType()
+                    .GetProperty(attr.EndpointPropertyName);
+
+                if (property == null)
+                {
+                    logger?.LogError($"Can not find property '{attr.EndpointPropertyName}'.");
+
+                    continue;
+                }
+
+                string propertyValue = property.GetValue(rabbitMqConfig)?.ToString();
+                if (string.IsNullOrEmpty(propertyValue))
+                {
+                    logger?.LogError($"RabbitMq config does not contain value for '{attr.EndpointPropertyName}'.");
+                }
+
+                busConfigurator.AddConsumer(injectConsumer);
+
+                configurator.ReceiveEndpoint(propertyValue, ep =>
+                {
+                    ep.ConfigureConsumer(context, injectConsumer);
+                });
+
+                logger?.LogTrace(
+                    $"Found comsumer type '{injectConsumer.Name}'. Successfully injected with endpoint '{propertyValue}'.");
             }
 
             return busConfigurator;
