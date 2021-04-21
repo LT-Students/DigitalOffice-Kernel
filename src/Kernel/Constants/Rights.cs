bfc989b805bb9011c10d2ea86eff883457f39ff9
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-04-21 13:50:31 +0300

    Feature/add rights news and positions (#22)
    * add constants to reghts
    
    * added nuspec
    
    * small fix

--- src/Kernel/Constants/Rights.cs
+++ src/Kernel/Constants/Rights.cs
@@ -6,5 +6,7 @@
         public const int AddEditRemoveProjects = 2;
         public const int AddEditRemoveEmailTemplates = 3;
         public const int AddEditRemoveDepartments = 4;
+        public const int AddEditRemoveNews = 5;
+        public const int AddEditRemovePositions = 6;
     }
 }
