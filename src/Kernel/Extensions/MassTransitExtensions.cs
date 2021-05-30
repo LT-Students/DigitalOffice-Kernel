b9d8cdefc5b0985decba3bc17cb9b525b91f2033
Egorka <belehov.egor2001@yandex.ru>
2021-05-30 17:57:12 +0300

    update auto inject requests

--- src/Kernel/Extensions/MassTransitExtensions.cs
+++ src/Kernel/Extensions/MassTransitExtensions.cs
@@ -4,7 +4,6 @@ using MassTransit.ExtensionsDependencyInjectionIntegration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -27,58 +26,22 @@ namespace LT.DigitalOffice.Kernel.Extensions
                 throw new ArgumentNullException(nameof(rabbitMqConfig));
             }
 
-            var asmPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
-            var files = Directory.GetFiles(asmPath, "*DigitalOffice*.dll");
-
-            List<Assembly> assemblies = new();
-
-            foreach (string fileName in files)
-            {
-                assemblies.Add(Assembly.LoadFrom(fileName));
-            }
-
-            List<Type> injectInterfaces = new();
-
-            foreach (Assembly assembly in assemblies)
-            {
-                injectInterfaces.AddRange(assembly.ExportedTypes
-                    .Where(
-                        t =>
-                            t.IsInterface
-                            && t.IsPublic
-                            && t.GetCustomAttribute(typeof(AutoInjectRequestAttribute)) != null)
-                    .ToList());
-            }
-
-            foreach (Type injectInterface in injectInterfaces)
-            {
-                var attr = injectInterface.GetCustomAttribute<AutoInjectRequestAttribute>();
-
-                PropertyInfo property = rabbitMqConfig
+            List<PropertyInfo> propertyInfos =
+                rabbitMqConfig
                     .GetType()
-                    .GetProperty(attr.EndpointPropertyName);
+                    .GetProperties()
+                    .Where(p => p.GetCustomAttribute(typeof(AutoInjectRequestAttribute)) != null).ToList();
 
-                if (property == null)
-                {
-                    logger?.LogError($"Can not find property '{attr.EndpointPropertyName}'.");
+            foreach(var property in propertyInfos)
+            {
+                var attr = property.GetCustomAttribute<AutoInjectRequestAttribute>();
 
-                    continue;
-                }
+                Uri endpointUri = new Uri($"{rabbitMqConfig.BaseUrl}/{property.GetValue(rabbitMqConfig)}");
 
-                string propertyValue = property.GetValue(rabbitMqConfig)?.ToString();
-                if (string.IsNullOrEmpty(propertyValue))
-                {
-                    logger?.LogError($"RabbitMq config does not contain value for '{attr.EndpointPropertyName}'.");
-
-                    continue;
-                }
-
-                Uri endpointUri = new Uri($"{rabbitMqConfig.BaseUrl}/{propertyValue}");
-
-                busConfigurator.AddRequestClient(injectInterface, endpointUri, attr.Timeout);
+                busConfigurator.AddRequestClient(attr.Model, endpointUri, attr.Timeout);
 
                 logger?.LogTrace(
-                    $"Found request type '{injectInterface.Name}'. Successfully injected with endpoint '{endpointUri}' and timeout '{attr.Timeout.Value}'.");
+                    $"Found request type '{attr.Model.Name}'. Successfully injected with endpoint '{endpointUri}' and timeout '{attr.Timeout.Value}'.");
             }
 
             return busConfigurator;
