30fa02eb190e6050fc66b441b0182803c6d63744
BelehovEgor <belehov.egor2001@yandex.ru>
2021-07-28 10:58:34 +0300

    start

---
+++ src/Kernel/Attributes/ParseEntityAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Kernel.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ParseEntityAttribute : Attribute
+    {
+        public List<string> IgnoredProperties { get; set; }
+
+        public ParseEntityAttribute(List<string> ignoredProperties)
+        {
+            IgnoredProperties = ignoredProperties;
+        }
+
+        public ParseEntityAttribute()
+        {
+            IgnoredProperties = new();
+        }
+    }
+}
