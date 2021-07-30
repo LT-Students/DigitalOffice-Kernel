309a2e5114056eda25ae3e198364cf435e2ff738
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-07-30 10:14:38 +0300

    add parse entities logic (#34)
    * start
    
    * add attributes and consumer
    
    * fix usings

---
+++ src/Kernel/Attributes/ParseEntity/IgnoreParseAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LT.DigitalOffice.Kernel.Attributes.ParseEntity
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IgnoreParseAttribute : Attribute
+    {
+    }
+}
---
+++ src/Kernel/Attributes/ParseEntity/Models/Requests/IFindParseEntitiesRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace LT.DigitalOffice.Kernel.Middlewares.ParseEntities.Models.Requests
+{
+    public interface IFindParseEntitiesRequest
+    {
+        static object CreateObj()
+        {
+            return new();
+        }
+    }
+}
