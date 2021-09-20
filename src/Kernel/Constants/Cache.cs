29643ff704c6467ae46e4c3d20dde95535cb0e5d
Misha <vasiljev.mish90@gmail.com>
2021-09-20 16:59:24 +0300

    fixes

---
+++ src/Kernel/Constants/Cache.cs
@@ -0,0 +1,10 @@
+﻿namespace LT.DigitalOffice.Kernel.Constants
+{
+    public static class Cache
+    {
+        public const int Users = 0;
+        public const int Positions = 1;
+        public const int Departments = 2;
+        public const int Projects = 3;
+    }
+}
