93104a608f4fe157c3d665d8fa61a997b883eafe
Misha <vasiljev.mish90@gmail.com>
2021-09-20 13:26:22 +0300

    init

---
+++ src/Kernel/Validators/BaseFindRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.Kernel.Validators.Interfaces;
+using LT.DigitalOffice.Kernel.Validators.Models;
+
+namespace LT.DigitalOffice.Kernel.Validators
+{
+    public class BaseFindRequestValidator : AbstractValidator<BaseFindRequest>, IBaseFindRequestValidator
+    {
+        public BaseFindRequestValidator()
+        {
+            RuleFor(fR => fR)
+                .Must(fR => fR.skipCount > -1)
+                .WithMessage("Skip count can't be less than 0.");
+
+            RuleFor(fR => fR)
+                .Must(fR => fR.takeCount > 0)
+                .WithMessage("Take count can't be less than 1.");
+        }
+    }
+}
---
+++ src/Kernel/Validators/Models/BaseFindRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.Kernel.Validators.Models
+{
+    public record BaseFindRequest
+    {
+        [FromQuery(Name = "skipcount")]
+        public int skipCount { get; set; }
+
+        [FromQuery(Name = "takecount")]
+        public int takeCount { get; set; }
+    }
+}
