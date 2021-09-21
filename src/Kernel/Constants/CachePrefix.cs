febdacffa07f2428a432fe0ae5b7d7c77bfa96b6
BelehovEgor <belehov.egor2001@yandex.ru>
2021-09-21 15:41:53 +0300

    add classes for redis

---
+++ src/Kernel/Constants/CachePrefix.cs
@@ -0,0 +1,9 @@
+﻿namespace LT.DigitalOffice.Kernel.Constants
+{
+    public static class CachePrefix
+    {
+        public static string Collectrion = "Collection_";
+
+        public static string Entity = "Entity_";
+    }
+}
---
+++ src/Kernel/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,24 @@
+﻿using LT.DigitalOffice.Kernel.Constants;
+using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Kernel.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static string GetRedisCacheHashCode(this IEnumerable<Guid> guids)
+        {
+            unchecked
+            {
+                int cache = 0;
+
+                foreach(Guid id in guids)
+                {
+                    cache += id.GetHashCode();
+                }
+
+                return $"{CachePrefix.Collectrion}{cache}";
+            }
+        }
+    }
+}
