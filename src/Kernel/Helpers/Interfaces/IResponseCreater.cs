3673d00e354c853720d38bcb4b02a626c5200553
BelehovEgor <belehov.egor2001@yandex.ru>
2021-10-08 15:34:57 +0300

    added response creator

---
+++ src/Kernel/Helpers/Interfaces/IResponseCreater.cs
@@ -0,0 +1,15 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
+using System.Collections.Generic;
+using System.Net;
+
+namespace LT.DigitalOffice.Kernel.Helpers.Interfaces
+{
+    [AutoInject]
+    public interface IResponseCreater
+    {
+        OperationResultResponse<T> CreateFailureResponse<T>(HttpStatusCode statusCode, List<string> errors = null);
+
+        FindResultResponse<T> CreateFailureFindResponse<T>(HttpStatusCode statusCode, List<string> errors = null);
+    }
+}
