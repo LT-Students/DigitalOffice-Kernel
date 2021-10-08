07b4fdecb976bbf68380ca2bd017ce26577d6da7
ndoriomedov <doriomedov213@gmail.com>
2021-10-08 17:56:10 +0300

    add classes for Images

---
+++ src/Kernel/Constants/ImageFormats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Immutable;
+
+namespace LT.DigitalOffice.Kernel.Constants
+{
+    public static class ImageFormats
+    {
+        public const string jpgFormat = ".jpg";
+        public const string jpegFormat = ".jpeg";
+        public const string pngFormat = ".png";
+        public const string bmpFormat = ".bmp";
+        public const string gifFormat = ".gif";
+        public const string tgaFormat = ".tga";
+        public const string svgFormat = ".svg";
+
+        public static ImmutableList<string> formats = ImmutableList
+          .Create(jpgFormat, jpegFormat, pngFormat, bmpFormat, gifFormat, tgaFormat, svgFormat);
+    }
+}
---
+++ src/Kernel/Validators/ImageContentValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.Kernel.Validators.Interfaces;
+using System;
+
+namespace LT.DigitalOffice.Kernel.Validators
+{
+    public class ImageContentValidator : AbstractValidator<string>, IImageContentValidator
+    {
+        public ImageContentValidator()
+        {
+            RuleFor(content => content)
+                .NotEmpty().WithMessage("Content can't be empty.");
+
+            When(content => !string.IsNullOrEmpty(content),
+            () =>
+            {
+                RuleFor(content => content)
+                    .Must(content => Convert.TryFromBase64String(content, new Span<byte>(new byte[content.Length]), out _))
+                    .WithMessage("Content must be base64 string");
+            });
+
+        }
+    }
+}
