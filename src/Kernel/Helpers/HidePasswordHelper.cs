5b02ca5b5140006dc50b63b34996418522b8153a
Nikita <cheephot4@gmail.com>
2021-10-16 00:32:56 +0300

    add HidePasswordHelper

---
+++ src/Kernel/Helpers/HidePasswordHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LT.DigitalOffice.Kernel.Helpers
+{
+    public static class HidePasswordHelper
+    {
+        public static string HidePassword(string line)
+        {
+            string password = "Password";
+
+            int index = line.IndexOf(password, 0, StringComparison.OrdinalIgnoreCase);
+
+            if (index != -1)
+            {
+                string[] words = Regex.Split(line, @"[=,; ]");
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (string.Equals(password, words[i], StringComparison.OrdinalIgnoreCase))
+                    {
+                        line = line.Replace(words[i + 1], "****");
+                        break;
+                    }
+                }
+            }
+
+            return line;
+        }
+    }
+}
