b8c9cee2c1fec50235cb62d2c4cda7dec9395c7b
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-10-19 11:46:05 +0300

    add class for cleaning redis cache (#53)
    * added cache notebook
    
    * merge develop
    
    * fix
    
    * rework new class
    
    * fix
    
    * fix

---
+++ src/Kernel/Configurations/RedisConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace LT.DigitalOffice.UserService.Models.Dto.Configurations
+{
+  public class RedisConfig
+  {
+    public const string SectionName = "Redis";
+
+    public double CacheLiveInMinutes { get; set; }
+  }
+}
--- src/Kernel/Helpers/Interfaces/IRedisHelper.cs
+++ src/Kernel/Helpers/Interfaces/IRedisHelper.cs
@@ -1,12 +1,11 @@
-﻿using LT.DigitalOffice.Kernel.Attributes;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.Kernel.Helpers.Interfaces
 {
-    public interface IRedisHelper
-    {
-        Task CreateAsync<T>(int database, string key, T item, TimeSpan? lifeTime = null);
-        Task<T> GetAsync<T>(int database, string key);
-    }
+  public interface IRedisHelper
+  {
+    Task CreateAsync<T>(int database, string key, T item, TimeSpan? lifeTime = null);
+    Task<T> GetAsync<T>(int database, string key);
+  }
 }
