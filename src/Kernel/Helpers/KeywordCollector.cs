d3e7522cb31494bb5949f2b1cbb9ee368d64f1ef
Mariya <mariyasin@list.ru>
2022-01-17 12:16:18 +0300

    addd attribute and helper

---
+++ src/Kernel/Helpers/KeywordCollector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using LT.DigitalOffice.Kernel.Attributes;
+
+namespace LT.DigitalOffice.Kernel.Helpers
+{
+  public static class KeywordCollector
+  {
+    public static Dictionary<int, List<string>> GetEndpointKeywords()
+    {
+      Dictionary<int, List<string>> endpointsKeywords = new();
+
+      IEnumerable<Type> assemblyTargets = AppDomain.CurrentDomain
+        .GetAssemblies()
+        .SingleOrDefault(assembly => assembly.GetName().Name.EndsWith("Models.Db"))
+        .ExportedTypes;
+
+      foreach (Type dbModel in assemblyTargets)
+      {
+        IEnumerable<PropertyInfo> properties = dbModel
+          .GetProperties()
+          .Where(p => p.GetCustomAttributes(typeof(KeywordAttribute), true).Any());
+
+        foreach (PropertyInfo property in properties)
+        {
+          foreach (int endpoint in
+            (property.GetCustomAttributes(typeof(KeywordAttribute), true).FirstOrDefault() as KeywordAttribute).Endpoints)
+          {
+            if (!endpointsKeywords.ContainsKey(endpoint))
+            {
+              endpointsKeywords.Add(endpoint, new List<string>());
+            }
+
+            endpointsKeywords[endpoint].Add(property.Name);
+          }
+        }
+      }
+
+      return endpointsKeywords;
+    }
+  }
+}
