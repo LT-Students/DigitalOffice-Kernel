2d7519c63504dbb4b3f78d394c0f06d4a8c56903
Mariya <63921256+MariyaSin@users.noreply.github.com>
2022-01-18 13:57:27 +0300

    addd attribute and helper (#61)

---
+++ src/Kernel/Attributes/KeywordAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LT.DigitalOffice.Kernel.Attributes
+{
+  [AttributeUsage(AttributeTargets.Property)]
+  public class KeywordAttribute : Attribute
+  {
+    public int[] Endpoints { get; }
+
+    public KeywordAttribute(params int[] endpoints)
+    {
+      Endpoints = endpoints;
+    }
+  }
+}
