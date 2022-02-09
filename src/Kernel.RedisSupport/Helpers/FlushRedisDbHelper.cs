cee419a4945ef7f873f8dead3304e68585803185
Nikita Doriomedov <88382379+ndoriomedov@users.noreply.github.com>
2022-02-09 21:37:03 +0300

    add redisFlush (#68)
    * add redisFlush
    
    * update version
    
    * remove serilog
    
    * fixes

---
+++ src/Kernel.RedisSupport/Helpers/FlushRedisDbHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using StackExchange.Redis;
+
+namespace LT.DigitalOffice.Kernel.RedisSupport.Helpers
+{
+  public static class FlushRedisDbHelper
+  {
+    public static string FlushDatabase(
+      string redisConnStr,
+      int database)
+    {
+      try
+      {
+        using (ConnectionMultiplexer cm = ConnectionMultiplexer.Connect(redisConnStr + ",allowAdmin=true,connectRetry=1,connectTimeout=2000"))
+        {
+          EndPoint[] endpoints = cm.GetEndPoints(true);
+
+          foreach (EndPoint endpoint in endpoints)
+          {
+            IServer server = cm.GetServer(endpoint);
+            server.FlushDatabase(database);
+          }
+        }
+
+        return null;
+      }
+      catch (Exception ex)
+      {
+        return $"Error while flushing Redis database №{database}. Text: {ex.Message}";
+      }
+    }
+  }
+}
