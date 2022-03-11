2fa5b40790fdd1a13285cb94e57e38db0162846c
ndoriomedov <doriomedov213@gmail.com>
2022-03-11 13:37:35 +0300

    change rights names

--- src/Kernel/Constants/Rights.cs
+++ src/Kernel/Constants/Rights.cs
@@ -4,15 +4,15 @@
   {
     public const int AddEditRemoveUsers = 1;
     public const int AddEditRemoveProjects = 2;
-    public const int AddEditRemoveEmailTemplates = 3;
+    public const int AddEditRemoveEmailsTemplates = 3;
     public const int AddEditRemoveDepartments = 4;
     public const int AddEditRemoveNews = 5;
     public const int AddEditRemovePositions = 6;
     public const int AddEditRemoveTime = 7;
     public const int AddEditRemoveHistories = 8;
-    public const int AddEditRemoveCompany = 9;
+    public const int AddEditRemoveCompanies = 9;
     public const int AddRemoveDepartmentData = 10;
     public const int AddEditRemoveCompanyData = 11;
-    public const int AddRemoveUserRole = 12;
+    public const int AddRemoveUsersRoles = 12;
   }
 }
