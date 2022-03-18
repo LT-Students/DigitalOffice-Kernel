7b2a0139b39d197e108d6e49653c0671d5439f1e
BelehovEgor <ebelekhov@foss.dk>
2022-03-18 20:31:04 +0300

    added data nuget

---
+++ src/Kernel.DataSupport/Database/Interfaces/IDataProvider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
+
+namespace LT.DigitalOffice.Kernel.DataSupport.Database.Interfaces
+{
+  /// <summary>
+  /// Base data provider interface.
+  /// </summary>
+  [AutoInject(Enums.InjectType.Scoped)]
+  public interface IDataProvider
+  {
+    /// <summary>
+    /// Async add new item.
+    /// </summary>
+    void Add<T>(T item) where T : class;
+
+    /// <summary>
+    /// ASync add collection of items.
+    /// </summary>
+    void AddRange<T>(IEnumerable<T> items) where T : class;
+
+    /// <summary>
+    /// Get collection.
+    /// </summary>
+    IQueryable<T> Get<T>() where T : class;
+
+    /// <summary>
+    /// Save data changes.
+    /// </summary>
+    void Save();
+    /// <summary>
+    /// Async save data changes.
+    /// </summary>
+    Task SaveAsync();
+    /// <summary>
+    /// Detach entity.
+    /// </summary>
+    object MakeEntityDetached(object obj);
+    /// <summary>
+    /// Ensure database deleted.
+    /// </summary>
+    void EnsureDeleted();
+    /// <summary>
+    /// Ensure database is in memory.
+    /// </summary>
+    bool IsInMemory();
+  }
+}
---
+++ src/Kernel.DataSupport/Database/ServiceDbContext.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.DataSupport.Database.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LT.DigitalOffice.Kernel.DataSupport.Database
+{
+  public class ServiceDbContext : DbContext, IDataProvider
+  {
+    public ServiceDbContext(DbContextOptions<ServiceDbContext> options)
+      : base(options)
+    {
+
+    }
+
+    // Fluent API is written here.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      var asmPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+      var files = Directory.GetFiles(asmPath, "LT.DigitalOffice.*.Models.Db.dll");
+
+      if (files.Length != 1)
+      {
+        throw new System.Exception();
+      }
+
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.LoadFrom(files.First()));
+    }
+
+    public object MakeEntityDetached(object obj)
+    {
+      Entry(obj).State = EntityState.Detached;
+
+      return Entry(obj).State;
+    }
+
+    public void Save()
+    {
+      SaveChanges();
+    }
+
+    public void EnsureDeleted()
+    {
+      Database.EnsureDeleted();
+    }
+
+    public bool IsInMemory()
+    {
+      return Database.IsInMemory();
+    }
+
+    public async Task SaveAsync()
+    {
+      await SaveChangesAsync();
+    }
+
+    public new void Add<T>(T item) where T : class
+    {
+      Set<T>().Add(item);
+    }
+
+    public void AddRange<T>(IEnumerable<T> items) where T : class
+    {
+      Set<T>().AddRange(items);
+    }
+
+    public IQueryable<T> Get<T>() where T : class
+    {
+      return Set<T>().AsQueryable();
+    }
+  }
+}
