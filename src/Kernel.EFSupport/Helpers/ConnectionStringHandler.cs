4906543294baf5ff1850c0e7cbc0ff35ddf24663
sparrowofnight@gmail.com <sparrowofnight@gmail.com>
2022-05-18 04:43:39 +0300

    fixed Serilog; removed IServiceCollection; and other changes according comments

--- src/Kernel.EFSupport/Helpers/ConnectionStringHandler.cs
+++ src/Kernel.EFSupport/Helpers/ConnectionStringHandler.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using LT.DigitalOffice.Kernel.Helpers;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 namespace LT.DigitalOffice.Kernel.EFSupport.Helpers
 {
   public static class ConnectionStringHandler
   {
-    public static string GetConnectionString(this IServiceCollection services, IConfiguration _configuration)
+    public static string GetConnectionString(IConfiguration configuration)
     {
       string connStr = Environment.GetEnvironmentVariable("ConnectionString");
 
       if (string.IsNullOrEmpty(connStr))
       {
-        connStr = _configuration.GetConnectionString("SQLConnectionString");
+        connStr = configuration.GetConnectionString("SQLConnectionString");
 
         Log.Information($"SQL connection string from appsettings.json was used. " +
           $"Value '{PasswordHider.Hide(connStr)}'.");
