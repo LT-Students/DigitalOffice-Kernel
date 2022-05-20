e2051235da8ebf1ca5c50290994aa62b78305889
vorobeva_marina <92924355+vorobevamv@users.noreply.github.com>
2022-05-20 15:01:50 +0300

    added dll Kernel.EFSupport (#83)
    * added dll Kernel.EFSupport (+nuspec+yml);
    IBaseDataProvider moved from Kernel/Database to Kernel.EFSupport/Provider
    http://outsourcing.nat.tepkom.ru:20080/redmine/issues/1048
    
    * comment removed
    
    * changes according comments
    
    * fixed Serilog;
    removed IServiceCollection;
    and other changes according comments
    
    * removed empty
    
    Co-authored-by: sparrowofnight@gmail.com <sparrowofnight@gmail.com>
    Co-authored-by: Mariya <mariyasin@list.ru>

---
+++ src/Kernel.EFSupport/Provider/IBaseDataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.Kernel.EFSupport.Provider
+{
+  /// <summary>
+  /// Base data provider interface.
+  /// </summary>
+  public interface IBaseDataProvider
+  {
+    /// <summary>
+    /// Save data changes.
+    /// </summary>
+    void Save();
+    /// <summary>
+    /// Async save data changes.
+    /// </summary>
+    Task SaveAsync();
+    /// <summary>
+    /// Detach entity.
+    /// </summary>
+    object MakeEntityDetached(object obj);
+    /// <summary>
+    /// Ensure database deleted.
+    /// </summary>
+    void EnsureDeleted();
+    /// <summary>
+    /// Ensure database is in memory.
+    /// </summary>
+    bool IsInMemory();
+  }
+}
