4fed949363aaede48eb9c599dedec57066324159
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-06-06 19:14:44 +0300

    implement custom model binder provider

---
+++ src/Kernel/CustomModelBinderProviders/CustomModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.CustomModelBinderProviders.Binders;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LT.DigitalOffice.Kernel.CustomModelBinderProviders
+{
+  public class CustomModelBinderProvider : IModelBinderProvider
+  {
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
+    {
+      if (context is null)
+        throw new ArgumentNullException(nameof(context));
+
+      Attribute trimAttribute = context.Metadata.ModelType.GetCustomAttribute<TrimAttribute>();
+
+      return trimAttribute != null
+        ? new StringTrimmerBinder()
+        : null;
+    }
+  }
+}
