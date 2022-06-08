8d216c911844ba06d97a979a65ebbbdf4d69d3d6
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-06-08 16:34:22 +0300

    Feature/add custom model binding for trimming (#86)

---
+++ src/Kernel/CustomModelBinderProviders/CustomModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.CustomModelBinderProviders.Binders;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LT.DigitalOffice.Kernel.CustomModelBinderProviders
+{
+  public class CustomModelBinderProvider : IModelBinderProvider
+  {
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
+    {
+      if (context is null)
+      {
+        throw new ArgumentNullException(nameof(context));
+      }
+
+      return context.Metadata.ModelType.GetCustomAttribute<TrimAttribute>() is not null
+        ? new StringTrimmerBinder()
+        : null;
+    }
+  }
+}
--- src/Kernel/Extensions/StringExtensions.cs
+++ src/Kernel/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace LT.DigitalOffice.Kernel.Extensions
@@ -51,6 +54,61 @@ namespace LT.DigitalOffice.Kernel.Extensions
       TimeSpan difference = DateTime.UtcNow - time;
 
       return $"{difference.Days} days {difference.Hours}h {difference.Minutes}m {difference.Seconds}s";
+    }
+
+    public static T TrimSpaces<T>(this T obj, Type modelType)
+    {
+      IEnumerable<PropertyInfo> properties = modelType
+        .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+        .Where(p => p.PropertyType == typeof(string) && p.CanWrite && p.CanRead);
+
+      foreach (PropertyInfo property in properties)
+      {
+        string value = (string)property.GetValue(obj);
+        if (value is not null)
+        {
+          object newValue = value.Trim();
+          property.SetValue(obj, newValue);
+        }
+      }
+
+      PropertyInfo baseTypeInfo = modelType
+        .GetProperties()
+        .FirstOrDefault(p => p.PropertyType.FullName != null && p.PropertyType.FullName.Contains("List"));
+      if (baseTypeInfo is not null)
+      {
+        object listValue = baseTypeInfo.GetValue(obj);
+
+        int listCount = (int)baseTypeInfo.PropertyType.GetProperty("Count")?.GetValue(listValue)!;
+        for (int innerIndex = 0; innerIndex < listCount; innerIndex++)
+        {
+          object item = baseTypeInfo.PropertyType
+            .GetMethod("get_Item", new[] { typeof(int) })
+            ?.Invoke(listValue, new object[] { innerIndex });
+          item?.TrimSpaces(item.GetType());
+        }
+      }
+
+      IEnumerable<PropertyInfo> customTypes =
+        modelType
+          .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+          .Where(
+            prop =>
+              prop.PropertyType.FullName != null
+              && !prop.PropertyType.IsPrimitive
+              && !prop.PropertyType.IsEnum
+              && prop.PropertyType.IsClass
+              && !prop.PropertyType.FullName.StartsWith("System"));
+
+      foreach (PropertyInfo customType in customTypes)
+      {
+        if (customType.GetIndexParameters().Length == 0)
+        {
+          customType.GetValue(obj).TrimSpaces(customType.GetType());
+        }
+      }
+
+      return obj;
     }
   }
 }
