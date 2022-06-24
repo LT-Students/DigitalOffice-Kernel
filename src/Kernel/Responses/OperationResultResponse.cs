85aa477889bfaba7ba12254c2372305f40644ae6
VLiamin <43515136+VLiamin@users.noreply.github.com>
2022-06-24 17:35:38 +0300

    Removed status from FindResultResponse and OperationResultResponse (#88)
    * removed status from FindResultResponse and OperationResultResponse
    
    * removed library
    
    * make ResponseCreator static
    
    * unite two methods
    
    * add comments
    
    * fixed some issues
    
    * return old ResponseCreator

--- src/Kernel/Responses/OperationResultResponse.cs
+++ src/Kernel/Responses/OperationResultResponse.cs
@@ -1,21 +1,17 @@
 ﻿using System.Collections.Generic;
-using LT.DigitalOffice.Kernel.Enums;
 
 namespace LT.DigitalOffice.Kernel.Responses
 {
   public class OperationResultResponse<T>
   {
     public T Body { get; set; }
-    public OperationResultStatusType Status { get; set; }
     public List<string> Errors { get; set; } = new();
 
     public OperationResultResponse(
       T body = default,
-      OperationResultStatusType status = default,
       List<string> errors = default)
     {
       Body = body;
-      Status = status;
       Errors = errors ?? new();
     }
   }
