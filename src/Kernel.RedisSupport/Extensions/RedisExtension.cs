fb104374f1f71625b07d598c2735d0a2383f25ea
ndoriomedov <doriomedov213@gmail.com>
2022-09-02 17:47:46 +0300

    update GetRedisCacheHashCode

--- src/Kernel.RedisSupport/Extensions/RedisExtension.cs
+++ src/Kernel.RedisSupport/Extensions/RedisExtension.cs
@@ -1,67 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LT.DigitalOffice.Kernel.RedisSupport.Extensions
 {
   public static class RedisExtension
   {
-    public static string GetRedisCacheHashCode(this IEnumerable<Guid> guids, params object[] additionalArguments)
+    private static int GetStringHashCode(this string arg)
+    {
+      if (!arg.Any())
+      {
+        return 0;
+      }
+
+      int hash = 17;
+      int coef = 31;
+
+      unchecked
+      {
+        foreach (char ch in arg)
+        {
+          hash += hash * coef + ch.GetHashCode();
+        }
+      }
+
+      return hash;
+    }
+
+    public static string GetRedisCacheHashCode(this IEnumerable<Guid> guids, params (string variableName, object value)[] additionalArguments)
     {
       unchecked
       {
-        int cache = 0;
-        int charPosition = 20;
+        int hashCode = 1;
 
         foreach (var id in guids)
         {
-          cache += id.GetHashCode();
+          hashCode += id.GetHashCode();
         }
 
         foreach (var arg in additionalArguments)
         {
-          if (arg is string)
+          if (arg.value is null)
           {
-            foreach (char value in arg.ToString().Replace(" ", ""))
-            {
-              cache += charPosition * value.GetHashCode();
-              charPosition++;
-            }
+            continue;
+          }
+
+          if (arg.value is string)
+          {
+            hashCode += hashCode * (arg.value.ToString().GetStringHashCode() + arg.variableName.GetStringHashCode());
           }
           else
           {
-            cache += arg.GetHashCode();
+            hashCode += hashCode * (arg.value.GetHashCode() + arg.variableName.GetStringHashCode());
           }
         }
 
-        return cache.ToString();
+        return hashCode.ToString();
       }
     }
 
-    public static string GetRedisCacheHashCode(this Guid id, params object[] additionalArguments)
+    public static string GetRedisCacheHashCode(this Guid id, params (string variableName, object value)[] additionalArguments)
     {
       unchecked
       {
-
-        int cache = id.GetHashCode();
-        int charPosition = 20;
+        int hashCode = 1 + id.GetHashCode();
 
         foreach (var arg in additionalArguments)
         {
-          if (arg is string)
+          if (arg.value is null)
           {
-            foreach (char value in arg.ToString().Replace(" ", ""))
-            {
-              cache += charPosition * value.GetHashCode();
-              charPosition++;
-            }
+            continue;
+          }
+
+          if (arg.value is string)
+          {
+            hashCode += hashCode * (arg.value.ToString().GetStringHashCode() + arg.variableName.GetStringHashCode());
           }
           else
           {
-            cache += arg.GetHashCode();
+            hashCode += hashCode * (arg.value.GetHashCode() + arg.variableName.GetStringHashCode());
           }
         }
 
-        return cache.ToString();
+        return hashCode.ToString();
       }
     }
   }
