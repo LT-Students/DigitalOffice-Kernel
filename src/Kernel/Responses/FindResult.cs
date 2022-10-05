2859359351dfed6d6935a40c18870e45095e6f88
sparrowofnight@gmail.com <sparrowofnight@gmail.com>
2022-10-05 23:30:17 +0300

    added FindResult

---
+++ src/Kernel/Responses/FindResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DigitalOffice.Kernel.Responses;
+
+public class FindResult<T>
+{
+  public List<T> Body { get; set; }
+  public int TotalCount { get; set; }
+
+  public FindResult(
+    List<T> body = default,
+    int totalCount = default)
+  {
+    Body = body;
+    TotalCount = totalCount;
+  }
+}
