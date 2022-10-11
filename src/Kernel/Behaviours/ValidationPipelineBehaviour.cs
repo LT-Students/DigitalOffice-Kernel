d2a2e4752bda262db760a6b08058a12047a174fb
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-10-11 21:02:22 +0300

    Feature/add validation behaviour (#100)

---
+++ src/Kernel/Behaviours/ValidationPipelineBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+using LT.DigitalOffice.Kernel.Exceptions.Models;
+using MediatR;
+
+namespace DigitalOffice.Kernel.Behaviours
+{
+  public class ValidationPipelineBehaviour<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+  {
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPipelineBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+      _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(
+      TRequest request,
+      CancellationToken cancellationToken,
+      RequestHandlerDelegate<TResponse> next)
+    {
+      if (!_validators.Any())
+      {
+        return await next();
+      }
+
+      ValidationContext<TRequest> context = new(request);
+      ValidationResult[] validationResults = await Task.WhenAll(
+        _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+      List<ValidationFailure> failures = validationResults
+        .SelectMany(r => r.Errors)
+        .Where(f => f != null)
+        .ToList();
+
+      if (failures.Count != 0)
+      {
+        throw new BadRequestException(failures.Select(f => f.ErrorMessage));
+      }
+
+      return await next();
+    }
+  }
+}
