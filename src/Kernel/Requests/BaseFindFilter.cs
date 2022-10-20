1360ebdc241c5aba5b766d8245be834120dde116
maybee16 <mayaochir@gmail.com>
2022-10-20 21:29:39 +0900

    Updated BaseFindFilter

--- src/Kernel/Requests/BaseFindFilter.cs
+++ src/Kernel/Requests/BaseFindFilter.cs
@@ -4,11 +4,35 @@ namespace LT.DigitalOffice.Kernel.Requests
 {
   public record BaseFindFilter
   {
+    private int _skipCount;
+
     [FromQuery(Name = "skipcount")]
-    public int SkipCount { get; set; }
+    public int SkipCount
+    {
+      get => _skipCount;
+      set
+      {
+        if (value > -1)
+        {
+          _skipCount = value;
+        }
+      }
+    }
+
+    private int _takeCount;
 
     [FromQuery(Name = "takecount")]
-    public int TakeCount { get; set; }
+    public int TakeCount 
+    { 
+      get => _takeCount; 
+      set
+      {
+        if (value > 0)
+        {
+          _takeCount = value;
+        }
+      }
+    }
 
     public BaseFindFilter(
       int skipCount = 0,
