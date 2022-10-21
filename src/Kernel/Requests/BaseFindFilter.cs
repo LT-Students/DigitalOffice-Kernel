3ba55e87ead36a308fa8af5946275f53cbcf2a6d
Maya O <55130636+maybee16@users.noreply.github.com>
2022-10-21 19:47:08 +0900

    Updated BaseFindFilter (#102)
    * Updated BaseFindFilter
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes

--- src/Kernel/Requests/BaseFindFilter.cs
+++ src/Kernel/Requests/BaseFindFilter.cs
@@ -4,18 +4,21 @@ namespace LT.DigitalOffice.Kernel.Requests
 {
   public record BaseFindFilter
   {
+    private int skipCount;
+    private int takeCount;
+
     [FromQuery(Name = "skipcount")]
-    public int SkipCount { get; set; }
+    public int SkipCount
+    {
+      get => skipCount;
+      set => skipCount = value > -1 ? value : 0;
+    }
 
     [FromQuery(Name = "takecount")]
-    public int TakeCount { get; set; }
-
-    public BaseFindFilter(
-      int skipCount = 0,
-      int takeCount = 1)
-    {
-      SkipCount = skipCount;
-      TakeCount = takeCount;
+    public int TakeCount 
+    { 
+      get => takeCount; 
+      set => takeCount = value > 0 ? value : 1;
     }
   }
 }
