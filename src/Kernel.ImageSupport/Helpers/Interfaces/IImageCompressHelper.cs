6dd3c7fb5640ffa3146f3da89b8add63d0388dd7
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-11-11 16:40:58 +0300

    fix typos

--- src/Kernel.ImageSupport/Helpers/Interfaces/IImageCompressHelper.cs
+++ src/Kernel.ImageSupport/Helpers/Interfaces/IImageCompressHelper.cs
@@ -14,16 +14,16 @@ public interface IImageCompressHelper
   /// Compresses image to given maximal value.
   /// </summary>
   /// <remarks>
-  /// Allows to compress images of following formats: ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tga", ".svg", ".webp", ".tiff", ".pbm", ".svg".
+  /// Allows to compress images of following formats: ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tga", ".svg", ".webp", ".tiff", ".pbm".
   /// Compresses images step by step until receiving required weight value.
   /// Svg image on first iteration converts to png image.
   /// Png, bmp, gif, tga, webp, tiff, pbm images on first iteration converts to jpeg(jpg).
   /// Jpeg(jpg) on first iteration compresses with quality 90%,
   /// then on each iteration reduces quality by 10% (second iteration - 80%, third - 70% and so one);
-  /// minimal quality value is 10% - compessing stops even if current weight is heigher then required.
-  /// If compressing is successfull, returns boolean "true", compressed image content and final extension.
+  /// minimal quality value is 10% - compressing stops even if current size is bigger then required.
+  /// If compressing is successful, returns boolean "true", compressed image content and final extension.
   /// If weight of original image is less than given maximal value, returns boolean "true", original image content and original extension.
-  /// If rcompressing is not successfull, returns boolean "false", null image content and original image extension.
+  /// If compressing is not successful, returns boolean "false", null image content and original image extension.
   /// </remarks>
   /// <param name="inputBase64">
   /// Image original content.
