4f16ea20f151618a3728f270f2e4cd1789ba97a7
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-11-16 17:34:29 +0300

    change summary from param to field

--- src/Kernel/Constants/Rights.cs
+++ src/Kernel/Constants/Rights.cs
@@ -4,17 +4,17 @@
   /// Rights give access to admin rights to the corresponding services or blocks
   /// Admin, Feedback, Email, create/update Roles only work with admin rights
   /// </summary>
-  /// <param name="AddEditRemoveUsers">Right grants admin rights to user, family, education, skill, office users, achievement </param>
-  /// <param name="AddEditRemoveProjects">Right grants admin rights to project, task services</param>
-  /// <param name="AddEditRemoveEmailsTemplates">Right grants admin rights to text template service</param>
-  /// <param name="AddEditRemoveDepartments">Right grants admin rights to department service, work with project department</param> 
-  /// <param name="AddEditRemoveNews">Right grants admin rights to news service</param> 
-  /// <param name="AddEditRemovePositions">Right grants admin rights to position service</param> 
-  /// <param name="AddEditRemoveTime">Right grants admin rights to time service</param> 
-  /// <param name="AddEditRemoveCompanies">Right grants admin rights to company, office services</param> 
-  /// <param name="AddEditRemoveCompanyData">Right grants admin rights to company service to work with company users, office workspaces/workspaceTypes, booking service</param> 
-  /// <param name="AddRemoveUsersRoles">Right grants admin rights to right service to work with users roles</param> 
-  /// <param name="AddEditRemoveWiki">Right grants admin rights to wiki service</param>
+  /// <field name="AddEditRemoveUsers">Right grants admin rights to user, family, education, skill, office users, achievement </field>
+  /// <field name="AddEditRemoveProjects">Right grants admin rights to project, task services</field>
+  /// <field name="AddEditRemoveEmailsTemplates">Right grants admin rights to text template service</field>
+  /// <field name="AddEditRemoveDepartments">Right grants admin rights to department service, work with project department</field>
+  /// <field name="AddEditRemoveNews">Right grants admin rights to news service</field>
+  /// <field name="AddEditRemovePositions">Right grants admin rights to position service</field>
+  /// <field name="AddEditRemoveTime">Right grants admin rights to time service</field>
+  /// <field name="AddEditRemoveCompanies">Right grants admin rights to company, office services</field>
+  /// <field name="AddEditRemoveCompanyData">Right grants admin rights to company service to work with company users, office workspaces/workspaceTypes, booking service</field>
+  /// <field name="AddRemoveUsersRoles">Right grants admin rights to right service to work with users roles</field>
+  /// <field name="AddEditRemoveWiki">Right grants admin rights to wiki service</field>
   public static class Rights
   {
     public const int AddEditRemoveUsers = 1;
