0f752af8d4e434339d4134dc86f902c62b2446b3
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-11-17 17:42:56 +0300

    fix rights docs

--- src/Kernel/Constants/Rights.cs
+++ src/Kernel/Constants/Rights.cs
@@ -4,17 +4,19 @@
   /// Rights give access to admin rights to the corresponding services or blocks
   /// Admin, Feedback, Email, create/update Roles only work with admin rights
   /// </summary>
-  /// <field name="AddEditRemoveUsers">Right grants admin rights to user, family, education, skill, office users, achievement </field>
-  /// <field name="AddEditRemoveProjects">Right grants admin rights to project, task services</field>
-  /// <field name="AddEditRemoveEmailsTemplates">Right grants admin rights to text template service</field>
-  /// <field name="AddEditRemoveDepartments">Right grants admin rights to department service, work with project department</field>
-  /// <field name="AddEditRemoveNews">Right grants admin rights to news service</field>
-  /// <field name="AddEditRemovePositions">Right grants admin rights to position service</field>
-  /// <field name="AddEditRemoveTime">Right grants admin rights to time service</field>
-  /// <field name="AddEditRemoveCompanies">Right grants admin rights to company, office services</field>
-  /// <field name="AddEditRemoveCompanyData">Right grants admin rights to company service to work with company users, office workspaces/workspaceTypes, booking service</field>
-  /// <field name="AddRemoveUsersRoles">Right grants admin rights to right service to work with users roles</field>
-  /// <field name="AddEditRemoveWiki">Right grants admin rights to wiki service</field>
+  /// <value name="AddEditRemoveUsers">Right grants admin rights to user, family, education, skill, office users, achievement </value>
+  /// <value name="AddEditRemoveProjects">Right grants admin rights to project, task services</value>
+  /// <value name="AddEditRemoveEmailsTemplates">Right grants admin rights to text template service</value>
+  /// <value name="AddEditRemoveDepartments">Right grants admin rights to department service, work with project department</value>
+  /// <value name="AddEditRemoveNews">Right grants admin rights to news service</value>
+  /// <value name="AddEditRemovePositions">Right grants admin rights to position service</value>
+  /// <value name="AddEditRemoveTime">Right grants admin rights to time service</value>
+  /// <value name="AddEditRemoveOffices">Right grants admin rights to office service</value>
+  /// <value name="AddEditRemoveCompanies">Right grants admin rights to company service</value>
+  /// <value name="AddEditRemoveBooking">Right grants admin rights to booking service</value>
+  /// <value name="AddEditRemoveCompanyData">Right grants admin rights to company service</value>
+  /// <value name="AddRemoveUsersRoles">Right grants admin rights to right service to work with users roles</value>
+  /// <value name="AddEditRemoveWiki">Right grants admin rights to wiki service</value>
   public static class Rights
   {
     public const int AddEditRemoveUsers = 1;
