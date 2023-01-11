b4fb4d734f3e0043531b3378cd2dd92d2e4c6498
AlfaRedDog <72893891+AlfaRedDog@users.noreply.github.com>
2023-01-11 14:59:13 +0300

    change namespace to LT.DigitalOffice.Kernel.Behaviours

--- src/Kernel/Behaviours/ValidationPipelineBehaviour.cs
+++ src/Kernel/Behaviours/ValidationPipelineBehaviour.cs
@@ -7,7 +7,7 @@ using FluentValidation.Results;
 using LT.DigitalOffice.Kernel.Exceptions.Models;
 using MediatR;
 
-namespace DigitalOffice.Kernel.Behaviours
+namespace LT.DigitalOffice.Kernel.Behaviours
 {
   public class ValidationPipelineBehaviour<TRequest, TResponse>
     : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
