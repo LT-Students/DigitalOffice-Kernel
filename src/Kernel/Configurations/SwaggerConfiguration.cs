d0186315f504ba64cd30a4a08035c018faccd793
ndoriomedov <nikita.doriomedov@lanit-tercom.com>
2023-06-30 15:20:01 +0300

    Add JsonPatchDocumentSchemaFilter, SwaggerConfiguration

---
+++ src/Kernel/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DigitalOffice.Kernel.Configurations;
+
+public class SwaggerConfiguration
+{
+  public const string SectionName = "Swagger";
+
+  private string servicePath = Environment.GetEnvironmentVariable("Service_Path");
+  public string ServicePath
+  {
+    get
+    {
+      return servicePath;
+    }
+    init
+    {
+      if (servicePath is null)
+      {
+        servicePath = value ?? string.Empty;
+      }
+    }
+  }
+}
---
+++ src/Kernel/OpenApi/SchemaFilters/JsonPatchDocumentSchemaFilter.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DigitalOffice.Kernel.OpenApi.SchemaFilters;
+
+public class JsonPatchDocumentSchemaFilter : ISchemaFilter
+{
+  public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+  {
+    if (context.Type == typeof(Operation[]))
+    {
+      Type argumentType = context.ParameterInfo.ParameterType.GetGenericArguments().FirstOrDefault();
+
+      if (argumentType is not null)
+      {
+        schema.Type = "array";
+
+        schema.Items = new OpenApiSchema
+        {
+          Type = "object",
+          Properties = new Dictionary<string, OpenApiSchema>
+          {
+            {
+              "op", new OpenApiSchema
+              {
+                Type = "string",
+                Enum = new List<IOpenApiAny> { new OpenApiString("replace") }
+              }
+            },
+            {
+              "value", new OpenApiSchema
+              {
+                Type = "object",
+                Nullable = true
+              }
+            },
+            {
+              "path", new OpenApiSchema
+              {
+                Type = "string",
+                Enum = argumentType.GetProperties().Select(p => new OpenApiString("/" + p.Name) as IOpenApiAny).ToList()
+              }
+            }
+          }
+        };
+
+        schema.Description = "Array of operations to perform.";
+      }
+    }
+  }
+}
