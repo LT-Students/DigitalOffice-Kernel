0d2cda792079255d7a3b5408a7c3aa5023c62096
Vladimir Liamin <LyaminVA@yandex.ru>
2024-01-09 16:22:08 +0300

    Add dapper support

---
+++ src/Kernel.DapperSupport/Extensions/DatabaseExtension.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator.Runner;
+using FluentMigrator.Runner.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace LT.DigitalOffice.Kernel.DapperSupport.Extensions;
+
+public static class DatabaseExtension
+{
+  public static IHost UpdateDatabase(this IHost host)
+  {
+    using IServiceScope scope = host.Services.CreateScope();
+    IMigrationRunner migrationService = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+    try
+    {
+      migrationService.ListMigrations();
+      migrationService.MigrateUp();
+    }
+    catch (MissingMigrationsException)
+    {
+      Log.Information("No migrations for dapper was found.");
+    }
+    catch (SqlException)
+    {
+      Log.Information("No migrations for dapper was found.");
+    }
+
+    return host;
+  }
+}
