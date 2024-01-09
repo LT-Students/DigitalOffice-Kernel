7df1681ad77e584a4e8a734a0318cf498e1fa92c
VLiamin <43515136+VLiamin@users.noreply.github.com>
2024-01-09 16:40:14 +0300

    Add dapper support (#141)
    * Add dapper support
    
    * small fix
    
    * small fix
    
    * replace yml

---
+++ src/Kernel.DapperSupport/Helpers/ConnectionStringHandler.cs
@@ -0,0 +1,29 @@
+﻿using LT.DigitalOffice.Kernel.Helpers;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+
+namespace LT.DigitalOffice.Kernel.DapperSupport.Helpers;
+
+public static class ConnectionStringHandler
+{
+  public static string Get(IConfiguration configuration)
+  {
+    string connStr = Environment.GetEnvironmentVariable("ConnectionString");
+
+    if (string.IsNullOrEmpty(connStr))
+    {
+      connStr = configuration.GetConnectionString("SQLConnectionString");
+
+      Log.Information($"SQL connection string from appsettings.json was used. " +
+        $"Value '{PasswordHider.Hide(connStr)}'.");
+    }
+    else
+    {
+      Log.Information($"SQL connection string from environment was used. " +
+        $"Value '{PasswordHider.Hide(connStr)}'.");
+    }
+
+    return connStr;
+  }
+}
