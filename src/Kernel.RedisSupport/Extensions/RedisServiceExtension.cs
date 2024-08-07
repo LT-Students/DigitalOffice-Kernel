3d0f8259b52d1f0b7a1c5eddabd42786c1552c9f
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2024-08-07 21:48:27 +0300

    add AddRedisDistributedCache extension method

--- src/Kernel.RedisSupport/Extensions/RedisServiceExtension.cs
+++ src/Kernel.RedisSupport/Extensions/RedisServiceExtension.cs
@@ -34,4 +34,40 @@ public static class RedisServiceExtension
 
     return redisConnectionString;
   }
+
+  /// <summary>
+  /// Registers Redis cache provider as distributed cache via <see cref="StackExchangeRedisCacheServiceCollectionExtensions"/>
+  /// </summary>
+  public static void AddRedisDistributedCache(this IServiceCollection services, IConfiguration configuration)
+  {
+    string redisConnectionString = Environment.GetEnvironmentVariable("RedisConnectionString");
+    if (string.IsNullOrEmpty(redisConnectionString))
+    {
+      redisConnectionString = configuration.GetConnectionString("Redis");
+
+      Log.Information($"Redis connection string from appsettings.json was used. Value '{PasswordHider.Hide(redisConnectionString)}'");
+    }
+    else
+    {
+      Log.Information($"Redis connection string from environment was used. Value '{PasswordHider.Hide(redisConnectionString)}'");
+    }
+
+    try
+    {
+      services.AddStackExchangeRedisCache(options =>
+      {
+        options.Configuration = redisConnectionString;
+        options.InstanceName = "Redis";
+
+        options.ConfigurationOptions = new ConfigurationOptions
+        {
+          AbortOnConnectFail = false, ConnectRetry = 1, ConnectTimeout = 2000
+        };
+      });
+    }
+    catch (Exception e)
+    {
+      Log.Error(e, "Failed to connect to Redis: {message}", e.Message);
+    }
+  }
 }
