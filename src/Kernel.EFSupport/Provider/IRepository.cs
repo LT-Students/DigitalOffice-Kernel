63612d5f7a7ef6d77988d1cf480dfe07898f7141
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-10-17 15:11:25 +0300

    Edit IRepository

--- src/Kernel.EFSupport/Provider/IRepository.cs
+++ src/Kernel.EFSupport/Provider/IRepository.cs
@@ -14,8 +14,8 @@ public interface IRepository<T> where T : class
   IQueryable<T> GetAllAsync();
   Task<T> GetAsync(Guid id);
   Task<(List<T>, int)> FindAsync(BaseFindFilter filter);
-  Task<Guid> CreateAsync(T entity);
+  Guid CreateAsync(T entity);
   Task<T> EditAsync(JsonPatchDocument<T> patch);
   Task<T> UpdateAsync(T entity);
-  Task<bool> DeleteAsync(T entity);
+  bool DeleteAsync(T entity);
 }
