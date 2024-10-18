1251ccd8742d248db21950a701daca6e99203095
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-10-18 14:13:29 +0300

    Edit IRepository

--- src/Kernel.EFSupport/Provider/IRepository.cs
+++ src/Kernel.EFSupport/Provider/IRepository.cs
@@ -11,11 +11,11 @@ namespace LT.DigitalOffice.Kernel.EFSupport.Provider;
 [AutoInject]
 public interface IRepository<T> where T : class
 {
-  IQueryable<T> GetAllAsync(bool toTrack);
+  IQueryable<T> GetAll(bool toTrack = true);
   Task<T> GetAsync(Guid id);
   Task<(List<T>, int)> FindAsync(BaseFindFilter filter);
-  Guid CreateAsync(T entity);
+  Guid Create(T entity);
   Task<T> EditAsync(JsonPatchDocument<T> patch);
   Task<T> UpdateAsync(T entity);
-  bool DeleteAsync(T entity);
+  bool Delete(T entity);
 }
