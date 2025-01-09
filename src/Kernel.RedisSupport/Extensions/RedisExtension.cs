e524fe694c2489b204effb159667f98edb8008b4
Ryzhenkova Ekaterina <93452892+ekaterinaryzhenkova@users.noreply.github.com>
2025-01-09 17:14:55 +0300

    Remove Newtonsoft.Json (#166)
    * remove Newtonsoft.Json
    
    * add some changes

--- src/Kernel.RedisSupport/Extensions/RedisExtension.cs
+++ src/Kernel.RedisSupport/Extensions/RedisExtension.cs
@@ -1,10 +1,10 @@
-﻿using Newtonsoft.Json;
-using Serilog;
+﻿using Serilog;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 
 namespace LT.DigitalOffice.Kernel.RedisSupport.Extensions;
 
@@ -112,7 +112,7 @@ public static class RedisExtension
       throw new ArgumentNullException(nameof(obj), "Null object for constructing cache key provided.");
     }
 
-    string cacheKey = JsonConvert.SerializeObject(obj);
+    string cacheKey = JsonSerializer.Serialize(obj);
     if (!string.IsNullOrEmpty(prefix))
     {
       cacheKey = $"{prefix}_{cacheKey}";
--- src/Kernel.RedisSupport/Helpers/RedisHelper.cs
+++ src/Kernel.RedisSupport/Helpers/RedisHelper.cs
@@ -1,12 +1,12 @@
 ﻿using LT.DigitalOffice.Kernel.RedisSupport.Constants;
 using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace LT.DigitalOffice.Kernel.RedisSupport.Helpers;
 
@@ -30,7 +30,7 @@ public class RedisHelper(
 
     return cache
       .GetDatabase((int)database)
-      .StringSetAsync(key, JsonConvert.SerializeObject(item), lifeTime);
+      .StringSetAsync(key, JsonSerializer.Serialize(item), lifeTime);
   }
 
   /// <inheritdoc/>
@@ -50,7 +50,7 @@ public class RedisHelper(
       return default;
     }
 
-    T item = JsonConvert.DeserializeObject<T>(data);
+    T item = JsonSerializer.Deserialize<T>(data);
 
     logger.LogInformation(
       "Cached value was received from cache {cache} with key {cacheKey}.",
@@ -120,7 +120,7 @@ public class RedisHelper(
 
     return item is string
       ? AddValueToSetAsync(db, key, item.ToString())
-      : AddValueToSetAsync(db, key, JsonConvert.SerializeObject(item));
+      : AddValueToSetAsync(db, key, JsonSerializer.Serialize(item));
   }
 
   /// <inheritdoc/>
@@ -147,7 +147,7 @@ public class RedisHelper(
 
     RedisValue value = item is string
       ? new RedisValue(item.ToString())
-      : new RedisValue(JsonConvert.SerializeObject(item));
+      : new RedisValue(JsonSerializer.Serialize(item));
 
     return db.SetRemoveAsync(new RedisKey(key), value);
   }
@@ -169,7 +169,7 @@ public class RedisHelper(
     {
       values[i] = items[i] is string
         ? new RedisValue(items[i].ToString())
-        : new RedisValue(JsonConvert.SerializeObject(items[i]));
+        : new RedisValue(JsonSerializer.Serialize(items[i]));
     }
 
     return db.SetRemoveAsync(new RedisKey(key), values);
@@ -185,7 +185,7 @@ public class RedisHelper(
 
     RedisValue value = item is string
       ? new RedisValue(item.ToString())
-      : new RedisValue(JsonConvert.SerializeObject(item));
+      : new RedisValue(JsonSerializer.Serialize(item));
 
     return db.SetContainsAsync(key, value);
   }
